142b5a4e4de8874926594c2daf8786170a512931
Shaan <shaan.erickson@chicagobooth.edu>
2023-01-16 17:22:14 -0600

    Added user show action

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root "photos#index"
 
   devise_for :users
+
+  get "/users/:id" => "users#show", as: :user
+
+  resources :users, only: :show
   
   resources :comments
   resources :follow_requests
